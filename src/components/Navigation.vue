d4da1168c7b3147f9062c8d5f8d46256154fc9f9
Patricia <patriciasteffen@hotmail.com>
2022-01-31 21:51:22 +0100

    start

---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template >
+  <div id="nav">Navigation Patricia</div>
+  <div>{{msg}}</div>
+</template>
+<script>
+// import firebase from "firebase/app";
+// import "firebase/auth";
+export default {
+  name: 'app',
+  components: {}
+}
+</script>
+<style >
+</style>
